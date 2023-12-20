6df7cfb751eeb3dc7548a89b87879be473badad1
FARDEEN AHMED MANSUR <92604855+imfrdn2001@users.noreply.github.com>
2023-12-21 02:45:57 +0530

    Update powertest.java

--- src/test/powertest.java
+++ src/test/powertest.java
@@ -1,26 +1,26 @@
 package powerpackage;
 
-
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-public class powertest {
+public class PowerTest {
     @Test
-    public void one_raised_to_one_is_onew() {
-        assert powerfinder.OF(1,1) == 1;
+    public void testOneRaisedToOneIsOne() {
+        assertEquals(1, PowerFinder.calculatePower(1, 1));
     }
 
     @Test
-    public void twoRaised_to_one_is_2() {
-        assert powerfinder.OF(2,1) == 2;
+    public void testTwoRaisedToOneIsTwo() {
+        assertEquals(2, PowerFinder.calculatePower(2, 1));
     }
 
     @Test
-    public void two_power_2_is_4() {
-        assert powerfinder.OF(2, 2) == 4;
+    public void testTwoPowerTwoIsFour() {
+        assertEquals(4, PowerFinder.calculatePower(2, 2));
     }
 
     @Test
-    public void power_of_2_and_3_Is_SIX() {
-        assert powerfinder.OF(3, 2) == 3*3;
+    public void testPowerOfThreeAndTwoIsNine() {
+        assertEquals(9, PowerFinder.calculatePower(3, 2));
     }
 }
